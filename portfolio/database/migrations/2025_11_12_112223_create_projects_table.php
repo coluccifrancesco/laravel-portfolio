91823d98853a8cfce11f39763fd71691f85a221b
coluccifrancesco <coluccif2002@gmail.com>
2025-11-13 14:44:06 +0100

    Implement Projects resource with controller, model, migration, seeder, and view updates

---
+++ portfolio/database/migrations/2025_11_12_112223_create_projects_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    //Run the migrations
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->json('tech_stack');
+            $table->string('img');
+            $table->string('repo_link');
+            $table->timestamps();
+        });
+    }
+
+    //Reverse the migrations
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ portfolio/database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+
+    // Run the database seeds.
+    public function run(Faker $faker): void {
+        
+        for ($i=0; $i < 6; $i++) { 
+            
+            $newProject = new Project();
+
+            $newProject->name = $faker->word();
+            $newProject->description = $faker->paragraph(5);
+            $newProject->tech_stack = json_encode($faker->words(5));
+            $newProject->img = $faker->word();
+            $newProject->repo_link = $faker->url();
+
+            $newProject->save();
+        }
+    }
+}
